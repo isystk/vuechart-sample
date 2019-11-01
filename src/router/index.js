823f60d2f9b2670dd606b4cacc239fd48a795fbd
iseyoshitaka <iseyoshitaka@team-lab.com>
2019-11-01 20:46:12 +0900

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from '@/components/Home'
+import VueChartJS from '@/components/VueChartJS'
+
+Vue.use(Router)
+
+export default new Router({
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/chartjs',
+      name: 'VueChartJS',
+      component: VueChartJS
+    }
+  ]
+})
